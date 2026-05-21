27792c7f84dbd82750a12ca91e3d7375dda86bd1
FatimaAlmurtadha <sukaina77150@gmail.com>
2026-05-21 16:19:41 +0200

    get an issue by its ID is tested and a new issue is created

--- practicingFastAPI/app/routs/issues.py
+++ practicingFastAPI/app/routs/issues.py
@@ -5,12 +5,13 @@ from app.storage import load_data, save_data
 
 router = APIRouter(prefix="/api/v1/issues", tags=["issues"])
 
-
+# get all issues
 @router.get("/", response_model=list[IssueOut])
 def get_issues():
     issues = load_data()
     return issues
 
+# post a new issue
 @router.post("/", response_model=IssueOut, status_code=status.HTTP_201_CREATED)
 def create_issue(payLoad: IssueCreate):
     """Create a new issue"""
@@ -26,3 +27,15 @@ def create_issue(payLoad: IssueCreate):
     issues.append(new_issue)
     save_data(issues)
     return new_issue
+
+# get an issue by its ID
+@router.get("/{issue_id}", response_model=IssueOut)
+def get_issue(issue_id: str):
+    """Retrive a specific issue by ID"""
+    issues = load_data()
+    for issue in issues:
+        if issue["id"] == issue_id:
+            return issue
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Issue not found")
+
+# 
